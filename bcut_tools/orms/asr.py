235de517192c987468b9718b7e867a84787ebc63
z0z0r4 <z0z0r4@outlook.com>
2025-11-17 00:38:57 +0800

    fix: 更新 ResultRspSchema 中 result 字段为可选类型

--- bcut_tools/orms/asr.py
+++ bcut_tools/orms/asr.py
@@ -1,5 +1,5 @@
 from enum import Enum
-
+from typing import Optional
 from pydantic import BaseModel
 
 
@@ -115,7 +115,7 @@ class ResultRspSchema(BaseModel):
     """任务结果查询响应"""
 
     task_id: str  # 任务id
-    result: str  # 结果数据-json
+    result: Optional[str] = None   # 结果数据-json，state 为 1 时 result 字段不存在
     remark: str  # 任务状态详情
     state: ResultStateEnum  # 任务状态
 
